d67d454e24facccabc1283fbe37ed47750114182
github username <github email address>
2018-01-11 21:20:41 -0500

    added some auth controller functionality

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,7 +1,13 @@
 class AuthController < ApplicationController
 
   def create
-    byebug
+    user = User.find_by(username: params[:username])
+
+    if user.authenticate(params[:password])
+      render json: {username: user.email, id: user.id}
+    else
+      render json: {error: 'User is invalid'}, status: 401
+    end
   end
-  
+
 end
