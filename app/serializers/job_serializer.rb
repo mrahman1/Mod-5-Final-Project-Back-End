9771796c651d948115bcd3cce78377bac1ecebc4
github username <github email address>
2018-01-17 14:39:00 -0500

    back end adjustments

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,7 +1,7 @@
 class JobSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :user_id, :title, :description, :candidates
   # belongs_to :user
-  # has_many :applications
+  has_many :applications
   # has_many :candidates, through: :applications
   has_many :candidates
 end
