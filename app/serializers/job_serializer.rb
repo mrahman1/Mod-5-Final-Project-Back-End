86ff40d1aa15d2806bb0b3486feb3dab633bf53f
github username <github email address>
2018-01-24 14:14:59 -0500

    adjusted job serioalizer

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,7 +1,10 @@
 class JobSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :user_id, :stages, :skills, :employment_type, :education_level, :position, :field, :status
+  attributes :id, :created_at, :formattedTime, :title, :description, :user_id, :stages, :skills, :employment_type, :education_level, :position, :field, :status, :application_ids
   # belongs_to :user
   has_many :applications, :dependent => :destroy
   # has_many :candidates, through: :applications
   #has_many :candidates
+  def formattedTime
+    object.created_at.strftime("%Y-%m-%d")
+  end
 end
